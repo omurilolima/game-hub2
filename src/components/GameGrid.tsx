b634d5f95400893583b35bdb9bf3e7c3c2c0b2aa
omurilolima <msl@cesar.school>
2025-06-16 13:01:08 +0100

    Filter Games by Genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,9 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
 	selectedGenre: Genre | null;
@@ -18,8 +18,8 @@ const GameGrid = ({ selectedGenre }: Props) => {
 			{error && <Text>{error}</Text>}
 			<SimpleGrid
 				columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-				spacing="10px"
-				padding={3}
+				padding="10px"
+				spacing={3}
 			>
 				{isLoading &&
 					skeletons.map((skeleton) => (
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -19,7 +16,7 @@ export interface Game {
 }
 
 const useGames = (selectedGenre: Genre | null) =>
-	useData<Game>("/games", { params: { genre: selectedGenre?.id } }, [
+	useData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
 		selectedGenre?.id,
 	]);
 
