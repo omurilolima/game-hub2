04d8713d035ce9951e88ec0240f8c43896ffd0cf
omurilolima <msl@cesar.school>
2025-06-23 19:10:55 +0100

    Fetching Genres with React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
+
+// install React Query: npm i @tanstack/react-query@4.28
+// install React Query Devtools: npm i @tanstack/react-query-devtools@4.28
+// import QueryClient, QueryClientProvider
+// import ReactQueryDevtools (blue icon)
+
+// create an instance of QueryClient
+//wrap the App with  <QueryClientProvider client={queryClient}>
+// add a <ReactQueryDevtools /> to our component tree
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<ChakraProvider theme={theme}>
 			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools />
+			</QueryClientProvider>
 		</ChakraProvider>
 	</React.StrictMode>
 );
