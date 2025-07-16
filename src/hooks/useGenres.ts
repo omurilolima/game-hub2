ca7a61d8132ab3eb14b79870535178323a56e7f6
omurilolima <msl@cesar.school>
2025-07-16 16:12:30 +0100

    Refactor interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
-import { Genre } from "../entities/Genre";
+import Genre from "../entities/Genre";
 
 // install MS for calculating time: npm i ms@2.1.3
 // install the type declaration for MS: npm i -D @types/ms
